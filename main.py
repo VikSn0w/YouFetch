8ee828c54bc90a046390700272cc8e33b5ae76f4
VikSn0w <36678730+VikSn0w@users.noreply.github.com>
2020-08-29 02:05:17 +0200

    Update 2.1.0
    Updated error handling regarding video resolution and fps selection (will give you an error if no matching format has been found)

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ except Exception as e:
 
 selection = "#"
 
-print("YouFetch 2.0.0 - viksn0w")
+print("YouFetch 2.1.0 - viksn0w")
 
 while selection != "Exit":
     inputURL = input("Insert the url of the video: ")
@@ -34,18 +34,25 @@ while selection != "Exit":
         videoWEBM = ytd.streams.filter(file_extension='webm', resolution=resRequested, fps=fpsRequested).first()
         videoMP4 = ytd.streams.filter(file_extension='mp4', resolution=resRequested, fps=fpsRequested).first()
 
+        status = 0xA113
+
         if  videoWEBM == None:
             print("WEBM not available... trying MP4...")
             if videoMP4 == None:
-                print("MP4 not available... try another format!")
+                print("MP4 not available... try another resolution or fps!")
+                status = 1
             else:
+                status = 0
                 print("MP4 found! Download started...")
                 videoMP4.download(filename=titleName) 
         else:
+            status = 0
             print("WEBM found! Download started...")
             videoWEBM.download(filename=titleName)
             cmd = 'rename '+titleName+'.webm '+titleName+'.mp4'
             subprocess.call(cmd, shell=True)
+
+        return status
             
     #------------------SubRoutines Block/End----------------------
 
@@ -61,72 +68,86 @@ while selection != "Exit":
 
     #------------------Audio and Video Download Block/Start-------------
     elif selection=="AV":
+        resultVideo = 0
         status = 0
-        while status == 0:
-            print("\n")
-            print("144p \n240p \n360p \n480p \n720p \n1080p \n1440p \n2160p")
-            
-            selection = input("What quality do you prefer? ")
-
-            fps = 0
-
-            if(selection != "144p" and selection != "240p" and selection != "360p" and selection != "480p" and selection != "720p" and selection != "1080p" and selection != "1440p" and selection != "2160p"):
-                print("Invalid selection!\n")
-                status = 0
-            else:
-                status = 1
+        print("break0")
+        while resultVideo == 0:
+            print("break1")
+            while status == 0:
                 print("\n")
-                while fps != "30fps" and fps != "60fps":
-                    fps = input("30fps or 60fps? ")
-                    if fps != "30fps" and fps != "60fps":
-                        print("Invalid selection!")
-                        
-        if   fps == "30fps": fps = 30
-        elif fps == "60fps": fps = 60
+                print("144p \n240p \n360p \n480p \n720p \n1080p \n1440p \n2160p")
+                
+                selection = input("What quality do you prefer? ")
 
-        print("\n")
-        downloadVideo(selection, fps, title)
+                fps = 0
 
-        print("\n")
-        downloadAudio("251", title)
+                if(selection != "144p" and selection != "240p" and selection != "360p" and selection != "480p" and selection != "720p" and selection != "1080p" and selection != "1440p" and selection != "2160p"):
+                    print("Invalid selection!\n")
+                    status = 0
+                else:
+                    status = 1
+                    print("\n")
+                    while fps != "30fps" and fps != "60fps":
+                        fps = input("30fps or 60fps? ")
+                        if fps != "30fps" and fps != "60fps":
+                            print("Invalid selection!")
+                            
+                if   fps == "30fps": fps = 30
+                elif fps == "60fps": fps = 60
 
-        cmd = 'ffmpeg -y -i '+title+'.mp3  -r 30 -i '+title+'.mp4  -filter:a aresample=async=1 -c:a flac -c:v copy '+title+'.mkv'
-        subprocess.call(cmd, shell=True)     
-        os.remove(title+".mp4")
-        os.remove(title+".mp3")
+            print("\n")
+            resultVideo = downloadVideo(selection, fps, title)
+            if resultVideo == 0:
+                downloadAudio("251", title)
 
-        cmd = 'ffmpeg -i '+title+'.mkv -strict -2 -codec copy '+title+'.mp4'
-        subprocess.call(cmd, shell=True)
+                cmd = 'ffmpeg -y -i '+title+'.mp3  -r 30 -i '+title+'.mp4  -filter:a aresample=async=1 -c:a flac -c:v copy '+title+'.mkv'
+                subprocess.call(cmd, shell=True)     
+                os.remove(title+".mp4")
+                os.remove(title+".mp3")
+
+                cmd = 'ffmpeg -i '+title+'.mkv -strict -2 -codec copy '+title+'.mp4'
+                subprocess.call(cmd, shell=True)
     #------------------Audio and Video Download Block/End------------
 
     #------------------Only Video Donwload Block/Start---------------
     elif selection=="V":
         print("V")
+
+        resultVideo = 0
         status = 0
-        while status == 0:
-            print("\n")
-            print("144p \n240p \n360p \n480p \n720p \n1080p \n1440p \n2160p")
-            
-            selection = input("What quality do you prefer? ")
-
-            fps = 0
-
-            if(selection != "144p" and selection != "240p" and selection != "360p" and selection != "480p" and selection != "720p" and selection != "1080p" and selection != "1440p" and selection != "2160p"):
-                print("Invalid selection!\n")
-                status = 0
-            else:
-                status = 1
+        print("break0")
+        while resultVideo == 0:
+            print("break1")
+            while status == 0:
                 print("\n")
-                while fps != "30fps" and fps != "60fps":
-                    fps = input("30fps or 60fps? ")
-                    if fps != "30fps" and fps != "60fps":
-                        print("Invalid selection!")
+                print("144p \n240p \n360p \n480p \n720p \n1080p \n1440p \n2160p")
+                
+                selection = input("What quality do you prefer? ")
 
-        if   fps == "30fps": fps = 30
-        elif fps == "60fps": fps = 60
+                fps = 0
 
-        print("\n")
-        downloadVideo(selection, fps, title)
+                if(selection != "144p" and selection != "240p" and selection != "360p" and selection != "480p" and selection != "720p" and selection != "1080p" and selection != "1440p" and selection != "2160p"):
+                    print("Invalid selection!\n")
+                    status = 0
+                else:
+                    status = 1
+                    print("\n")
+                    while fps != "30fps" and fps != "60fps":
+                        fps = input("30fps or 60fps? ")
+                        if fps != "30fps" and fps != "60fps":
+                            print("Invalid selection!")
+                            
+                if   fps == "30fps": fps = 30
+                elif fps == "60fps": fps = 60
+
+            
+            print("\n")
+            resultVideo = downloadVideo(selection, fps, title)
+            if resultVideo == 0:
+                os.remove(title+".mp4")
+
+                cmd = 'ffmpeg -i '+title+'.mkv -strict -2 -codec copy '+title+'.mp4'
+                subprocess.call(cmd, shell=True)
     #------------------Only Video Donwload Block/End---------------
        
     elif selection == "Exit":
